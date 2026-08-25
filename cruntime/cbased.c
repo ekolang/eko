97cbc535582e6a43cbf9ba13bcc563f18e13f164
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-08-25 11:39:22 +0330

    Add cbased

---
+++ cruntime/cbased.c
@@ -0,0 +1,60 @@
+#include "cbased.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+void* _calloc(size_t num, size_t size)
+{
+    void* al = calloc(num, size);
+    if (al == NULL)
+    {
+        return 0;
+    } else {
+        return al;
+    }
+}
+
+
+void _abort()
+{
+	exit(0);
+}
+
+//void _atexit()
+//{
+
+//}
+
+//atexit must define in compiler itself
+
+int _abs(int num)
+{
+	return abs(num);
+}
+
+double _acos(double arg)
+{
+	if (arg <= -1.0 && arg >= 1.0){
+		return acos(arg);
+	} else {
+		return 0.0;
+	}
+}
+double _asin(double asina)
+{
+	if (asina <= -1.0)
+	{
+		if (asina >= 1.0){
+			return asin(asina);
+		} else {
+			return 0.0;
+		}
+	} else {
+		return 0.0;
+	}
+}
+
+double _atan2(double x, double y)
+{
+	return _atan2(x, y);
+}
---
+++ cruntime/cbased.h
@@ -0,0 +1,13 @@
+#ifdef CBASED_H
+#define CBASED_H
+
+#include <stddef.h>
+
+void* _calloc(size_t num, size_t size);
+void _abort();
+int _abs(int num);
+double _acos(double arg);
+double _atan2(double x, double y);
+double _asin(double asina);
+
+#endif
