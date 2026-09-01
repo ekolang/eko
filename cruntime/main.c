8b92969a378e7baa28943e73c45ff0e023b9cf6d
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-09-01 16:26:46 +0330

    Update

---
+++ cruntime/main.c
@@ -0,0 +1,23 @@
+#include "cbased.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// memory part
+
+void* _calloc(size_t num, size_t size)
+{
+    int al = (int*)calloc(num, size);
+    if (al == NULL)
+    {
+        return 0;
+    } else {
+        retunr al;
+    }
+}
+
+
+void *_realloc(void *ptr, unsigned size)
+{
+
+}
